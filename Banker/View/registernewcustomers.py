ab3d19f2eb55c3a4f6a240392c11976d02d74e54
MaduMitha-Ravi <63928819+MaduMitha-Ravi@users.noreply.github.com>
2021-03-16 14:47:54 -0500

    Update registernewcustomers.py

--- Banker/View/registernewcustomers.py
+++ Banker/View/registernewcustomers.py
@@ -8,6 +8,7 @@ from Banker.models import RegisterCustomers
 from django.contrib import messages
 from django.contrib.auth import get_user_model
 from django.db import connection
+from Banker.View.bankerlogin import bankerlogin
 
 
 class registernewcustomers:
@@ -27,13 +28,17 @@ class registernewcustomers:
 					first_name = request.POST.get('firstname')
 					last_name = request.POST.get('lastname')
 					email = request.POST.get('email')
+					if bankerlogin.is_user_customer(request, username) == 0:
+						user = UserModel.objects.create_user(username=username, password = password, first_name=first_name, last_name=last_name, email = email)
+						user.save()
+						output = form.save()
+						messages.success(request, ('Customer Registration successful! Customer ID is %s') % output.account_id)
 
-					user = UserModel.objects.create_user(username=username, password = password, first_name=first_name, last_name=last_name, email = email)
-					user.save()
-					output = form.save()
-					messages.success(request, ('Customer Registration successful! Customer ID is %s') % output.account_id)
-
-					return render(request, 'Banker/registercustomer.html', {'form': form})
+						return render(request, 'Banker/registercustomer.html', {'form': form})
+					else:
+						messages.success(request, ('Username %s is not available') % output.username)
+						#return render(request, 'Banker/registercustomer.html', {'form': form})
+					
 			elif 'reset' in request.POST:
 				#form = RegisterCustomerForm()
 				return HttpResponseRedirect(request.path)
