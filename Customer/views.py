bb321af08a91be5c85729618de82d7431e6e5799
MaduMitha-Ravi <63928819+MaduMitha-Ravi@users.noreply.github.com>
2021-03-19 00:07:50 -0500

    Update views.py

--- Customer/views.py
+++ Customer/views.py
@@ -101,8 +101,9 @@ class Customer:
         context = { 'table': table ,  'table1': table1 }
         
         loan_status = CustomerValidation.get_loan_latest_status(request)
+	messages.INFO(request, ' %s ' % loan_status)
 	#print(loan_status)
-        messages.INFO(request, (' %s ') % loan_status)
+        #messages.INFO(request, (' %s ') % loan_status)
 					
         return render(request, 'Customer/customermainpage.html', context=context)    
 
