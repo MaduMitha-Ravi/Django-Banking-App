08cb9ede085e6833bbd7c187e93e2043e5a5d0a2
MaduMitha-Ravi <63928819+MaduMitha-Ravi@users.noreply.github.com>
2021-03-22 16:21:43 -0500

    Update settings.py

--- SEBankingProject/settings.py
+++ SEBankingProject/settings.py
@@ -25,9 +25,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ssoftwareengineering@gmail.com'
 EMAIL_HOST_PASSWORD = 'forseproject'
-#EMAIL_PORT = 587
+EMAIL_PORT = 587
 #EMAIL_USE_SSL = True
-EMAIL_PORT = 465
+#EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 #DEFAULT_FROM_EMAIL = 'default from email'
 
