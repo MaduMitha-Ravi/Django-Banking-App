89348c5e3565026cbe19b2fed2964359650775e1
MaduMitha-Ravi <63928819+MaduMitha-Ravi@users.noreply.github.com>
2021-03-22 20:52:28 -0500

    Update settings.py

--- SEBankingProject/settings.py
+++ SEBankingProject/settings.py
@@ -38,7 +38,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'ssoftwareengineering@gmail.com'
 EMAIL_HOST_PASSWORD = 'forsoftwarenggproject'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = 'ssoftwareengineering@gmail.com'
 
