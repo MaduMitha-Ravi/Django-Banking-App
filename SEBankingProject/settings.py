329e24f0e1b8ad7f10ea2b499668eadf73f92dfb
MaduMitha-Ravi <63928819+MaduMitha-Ravi@users.noreply.github.com>
2021-06-08 09:28:23 -0500

    Update settings.py

--- SEBankingProject/settings.py
+++ SEBankingProject/settings.py
@@ -100,16 +100,7 @@ WSGI_APPLICATION = 'SEBankingProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'SEProject',
-        'USER':'postgres',
-        'PASSWORD':'newtothis', 
-        'HOST':'localhost',
-        'PORT': ''
-    }
-}
+
 
 import dj_database_url
 
